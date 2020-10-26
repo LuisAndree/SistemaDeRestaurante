6f5d8d7ff5820b4197d9c86121c81860e1cc1781
luisandree <andre2016999@gmail.com>
2020-10-26 12:18:34 -0300

    alteraçôes no app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,86 @@
 <template>
   <div class="corpo">
-    <h1 class="estiloTitulo">{{ titulo }}</h1>
-    <h2 class="estiloTitulo2">{{ menu }}</h2>
-    <form>
-      <labe>
-        <input placeholder="nome do prato">
-      </labe>
-    </form>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input
+      type="search"
+      class="filtro"
+      @input = "filtro = $event.target.value"
+      placeholder="Pesquise aqui o prato"
+    />
+    {{filtro}}
+    <ul class="lista-pratos">
+      <li class="lista-pratos-item" v-for="prato of pratosComFiltro">
+        <meu-painel :titulo="prato.titulo">
+          <img class="imagem-responciva" :src="prato.url" :alt="prato.titulo" />
+        </meu-painel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import PainelPrato from "../src/components/shared/painelPrato/PainelPrato.vue";
 export default {
-  name: "app",
+  components: {
+    "meu-painel": PainelPrato
+  },
+
   data() {
     return {
       titulo: "Cardapio",
-      menu: "Menu de opções"
+      pratos: [],
+      filtro: '',
     };
+  },
+
+  computed: {
+    pratosComFiltro() {
+      if (this.filtro){
+        let exp  = new RegExp(this.filtro.trim(), 'i');
+        return this.pratos.filter(prato => exp.test(prato.titulo));
+      } else {
+        return this.pratos;
+      }
+    }
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/pratos")
+      .then(res => res.json())
+      .then(
+        pratos => (this.pratos = pratos),
+        err => console.log(err)
+      );
   }
 };
 </script>
 
-<style>
-.corpo {
-  background-color: white;
+<style scoped>
+.centralizado {
+  text-align: center;
 }
 
-.estiloTitulo {
-  color: #755423;
-  text-align: center;
-  background-color: coral;
-  width: 100%;
-  height: 50px;
-  font-family: "Times New Roman", Times, serif;
-  font-size: 45px;
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0% auto;
+  width: 96%;
 }
-.estiloTitulo2 {
-  color: #755423;
-  text-align: center;
-  background-color: white;
-  width: center;
-  height: 40px;
-  font-family: "Times New Roman", Times, serif;
-  font-size: 30px;
+
+.lista-pratos {
+  list-style: none;
+}
+
+.lista-pratos .lista-pratos-item {
+  display: inline-block;
+}
+
+.imagem-responciva {
+  width: 100%;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
 }
 </style>
--- src/components/shared/painelPrato/PainelPrato.vue
+++ src/components/shared/painelPrato/PainelPrato.vue
@@ -1,25 +1,45 @@
 <template>
-<div>
-  <h1 class="estiloPinel">{{}}</h1>
-</div>
+  <div class="painel">
+    <h2 class="painel-titulo" @dblclick="visivel || !visivel">{{ painel.titulo }}</h2>
+    <div class="painel-conteudo" v-show="visivel">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
+export default {
+  props: ['titulo'],
 
-
-export default{
-
-}
+  data() {
+    return {
+      visivel: true
+    }
+  },
+};
 </script>
 
 <style scoped>
-.estiloPainel {
-  padding: auto;
-  border: solid, 2px;
-  text-align: center;
-  margin: 5px;
-  background-color: teal;
-  display: inline;
 
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 0%;
+  text-transform: uppercase;
 }
 </style>
